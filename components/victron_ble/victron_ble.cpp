67a1759c09531807b4b3e0dbb7768c792aa3d61b
Kevin Zimmermann <Kevinzimmermann94@gmx.de>
2025-12-10 22:55:30 +0100

    test_V05

--- components/victron_ble/victron_ble.cpp
+++ components/victron_ble/victron_ble.cpp
@@ -316,7 +316,7 @@ bool VictronBle::is_record_type_supported_(const VICTRON_BLE_RECORD_TYPE record_
       expected_len = sizeof(VICTRON_BLE_RECORD_ORION_XS);
       break;
     default:
-      ESP_LOGW(TAG, "[%s] Unsupported record type %02X", this->address_str().c_str(), (u_int8_t) record_type);
+      ESP_LOGW(TAG, "[%s] Unsupported record type 0x%02X", this->address_str().c_str(), (u_int8_t) record_type);
       return false;
       break;
   }
