a454576f1a061c881653763004f2d3a1bc5317fd
Kevin Zimmermann <Kevinzimmermann94@gmx.de>
2026-01-23 22:55:06 +0100

    Rename SOC field to charge_level in VICTRON_BLE_RECORD_SMART_BMS structure

--- components/victron_ble/victron_ble.h
+++ components/victron_ble/victron_ble.h
@@ -830,7 +830,7 @@ struct VICTRON_BLE_RECORD_SMART_BMS {  // NOLINT(readability-identifier-naming,a
   vic_16bit_0_01_positive battery_voltage;
   vic_16bit_0_01 system_plus_voltage;
   vic_16bit_0_1 alternator_voltage;
-  vic_7bit_1 soc;
+  vic_7bit_1 charge_level;
   // TODO
   u_int16_t io_status;
   // TODO
