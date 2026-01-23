71b28f19df299ee5c1c70ce5f035a5eaa9d3f391
Kevin Zimmermann <Kevinzimmermann94@gmx.de>
2026-01-23 21:08:19 +0100

    Update system_plus_voltage type in VICTRON_BLE_RECORD_SMART_BMS structure

--- components/victron_ble/victron_ble.h
+++ components/victron_ble/victron_ble.h
@@ -828,7 +828,7 @@ struct VICTRON_BLE_RECORD_SMART_BMS {  // NOLINT(readability-identifier-naming,a
   // TODO
   u_int8_t error;
   vic_16bit_0_01_positive battery_voltage;
-  vic_12bit_0_01_positive system_plus_voltage;
+  vic_16bit_0_01 system_plus_voltage;
   vic_16bit_0_1 alternator_voltage;
   vic_10bit_0_1_positive soc : 10;
   // TODO
