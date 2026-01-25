34c55f60fcb41008ffc8eba7c198ec825721a63e
Kevin Zimmermann <Kevinzimmermann94@gmx.de>
2026-01-25 10:24:40 +0100

    Refactor battery_voltage and charge_level types in VICTRON_BLE_RECORD_SMART_BMS structure

--- components/victron_ble/victron_ble.h
+++ components/victron_ble/victron_ble.h
@@ -827,10 +827,10 @@ struct VICTRON_BLE_RECORD_LYNX_SMART_BMS {  // NOLINT(readability-identifier-nam
 struct VICTRON_BLE_RECORD_SMART_BMS {  // NOLINT(readability-identifier-naming,altera-struct-pack-align)
   // TODO
   u_int8_t error;
-  vic_16bit_0_01_positive battery_voltage;
+  vic_16bit_0_01 battery_voltage;
   vic_16bit_0_01 system_plus_voltage;
   vic_16bit_0_01 alternator_voltage;
-  vic_10bit_0_1_positive charge_level;
+  vic_7bit_1 charge_level;
   // TODO
   u_int16_t io_status;
   // TODO
